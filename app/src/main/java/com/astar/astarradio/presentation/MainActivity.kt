8f2829a14b19f3750ae27208d7bd1a8c683c6f8c
Astar76 <astar.in.black@mail.ru>
2022-04-22 13:14:00 +0300

    Init Project

---
+++ app/src/main/java/com/astar/astarradio/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.astar.astarradio.presentation
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.astar.astarradio.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
